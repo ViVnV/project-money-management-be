c8d5ac3ea60aa501b5844c902e5d5d02dc8014dc
Vincent <vincent.ptk17@gmail.com>
2024-06-28 16:04:34 +0800

    Update controller.js
    penyelesaian CRUD ( kasaran )

--- controllers.js
+++ controllers.js
@@ -1,4 +1,5 @@
 const { PrismaClient } = require('@prisma/cleint');
+const { get } = require('./routes');
 const prisma = new PrismaClient();
 
 async function createFinancialRecords(req, res) {
@@ -32,6 +33,75 @@ async function createFinancialRecords(req, res) {
         res.status(201).send(newRecords);
     } catch (error) {
         console.log(error);
-        res.status(500).send({ message: "Failed to create financial records"});
+        res.status(500).send({ message: "Failed to create financial records" });
     }
+}
+
+async function getFinancialRecords(req, res) {
+    // Read all financial records
+    try {
+        const records = await prisma.FinancialRecords.findMany();
+        res.send(records);
+    } catch (error) {
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+}
+
+async function updateFinancialRecords(req, res) {
+    const { newTitle, amount, date, category, description } = req.body;
+
+    try {
+        const financialRecord = await prisma.FinancialRecords.findUnique ({
+            where: { title: title},
+        });
+
+        if (!financialRecord) {
+            return res.status(400).json({ error: "Financial Records Not Found!"})
+        }
+
+        const updatedFinancialRecord = await prisma.FinancialRecords.update({
+            where: { title: title },
+            data: {
+                title: newTitle || title,
+                amount: amount !== undefined ? amount : financialRecord.amount,
+                date: date ? new Date(date) : financialRecord.date,
+                category: category !== undefined ? category : financialRecord.category,
+                description: description !== undefined ? description : financialRecord.description,
+            },
+        });
+
+        return res.status(200).json(updatedFinancialRecords);
+    } catch (error) { 
+        console.error("Error updating financial record:", error);
+        return res.status(500).json({ error: "Something went wrong while updating financial record"})
+    }
+}
+
+async function deleteFinancialRecords(req, res) {
+    const financialRecordId = parseInt(req.params.id);
+    
+    try {
+        const financialRecord = await prisma.FinancialRecords.findUnique({
+            where: { id: financialRecordId },
+        });
+
+        if (!financialRecord) {
+            return res.status(404).send({ message: "Financial Record Not Found" });
+        }
+
+        await prisma.book.delete({
+            where: { id: financialRecordId },
+        });
+
+        res.send({ message: "Financial Record has been deleted successfully" });
+    } catch (error) {
+        res.status(500).send({ message: "Something went wrong while deleting financial record" });
+    }
+}
+
+module.exports = {
+    createFinancialRecords,
+    getFinancialRecords,
+    updateFinancialRecords,
+    deleteFinancialRecords,
 }
